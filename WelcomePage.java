150e8af2bf29e07801a9d94684163dddbb6f71bd
ayanaggarwal006 <89088576+ayanaggarwal006@users.noreply.github.com>
2023-08-15 18:15:47 +0530

    Update WelcomePage.java

--- WelcomePage.java
+++ WelcomePage.java
@@ -22,7 +22,6 @@ public class WelcomePage extends javax.swing.JFrame {
 
    
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
@@ -39,7 +38,7 @@ public class WelcomePage extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(248, 148, 6));
 
-        jLabelClose.setFont(new java.awt.Font("Ubuntu Medium", 1, 24)); // NOI18N
+        jLabelClose.setFont(new java.awt.Font("Ubuntu Medium", 1, 24)); 
         jLabelClose.setForeground(new java.awt.Color(254, 254, 254));
         jLabelClose.setText("X");
         jLabelClose.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
@@ -49,7 +48,7 @@ public class WelcomePage extends javax.swing.JFrame {
             }
         });
 
-        jLabelMin.setFont(new java.awt.Font("Ubuntu Medium", 1, 24)); // NOI18N
+        jLabelMin.setFont(new java.awt.Font("Ubuntu Medium", 1, 24));
         jLabelMin.setForeground(new java.awt.Color(255, 250, 250));
         jLabelMin.setText("-");
         jLabelMin.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
@@ -83,17 +82,17 @@ public class WelcomePage extends javax.swing.JFrame {
         jPanel2.setBackground(new java.awt.Color(44, 62, 80));
 
         jLabel1.setBackground(new java.awt.Color(236, 240, 241));
-        jLabel1.setFont(new java.awt.Font("Ubuntu Light", 1, 24)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Ubuntu Light", 1, 24));
         jLabel1.setForeground(new java.awt.Color(254, 254, 254));
         jLabel1.setText("Financial Nexus");
 
         jLabel6.setBackground(new java.awt.Color(236, 240, 241));
-        jLabel6.setFont(new java.awt.Font("Ubuntu Light", 1, 36)); // NOI18N
+        jLabel6.setFont(new java.awt.Font("Ubuntu Light", 1, 36)); 
         jLabel6.setForeground(new java.awt.Color(254, 254, 254));
         jLabel6.setText("WELCOME");
 
         jButton1.setBackground(new java.awt.Color(1, 1, 1));
-        jButton1.setFont(new java.awt.Font("Ubuntu Medium", 1, 14)); // NOI18N
+        jButton1.setFont(new java.awt.Font("Ubuntu Medium", 1, 14)); 
         jButton1.setForeground(new java.awt.Color(0, 0, 0));
         jButton1.setText("LOG  IN");
         jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -103,7 +102,7 @@ public class WelcomePage extends javax.swing.JFrame {
         });
 
         jButtonSignup.setBackground(new java.awt.Color(5, 1, 0));
-        jButtonSignup.setFont(new java.awt.Font("Ubuntu Medium", 1, 14)); // NOI18N
+        jButtonSignup.setFont(new java.awt.Font("Ubuntu Medium", 1, 14)); 
         jButtonSignup.setForeground(new java.awt.Color(0, 0, 0));
         jButtonSignup.setText("SIGN UP");
         jButtonSignup.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -167,37 +166,37 @@ public class WelcomePage extends javax.swing.JFrame {
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }
 
-    private void jLabelCloseMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabelCloseMouseClicked
+    private void jLabelCloseMouseClicked(java.awt.event.MouseEvent evt) {
         System.exit(0);
-    }//GEN-LAST:event_jLabelCloseMouseClicked
+    }
 
-    private void jLabelMinMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabelMinMouseClicked
+    private void jLabelMinMouseClicked(java.awt.event.MouseEvent evt) {
         this.setState(JFrame.ICONIFIED);
-    }//GEN-LAST:event_jLabelMinMouseClicked
+    }
 
-    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {
         LoginPage lp=new LoginPage();
         lp.setVisible(true);
         lp.pack();
         lp.setLocationRelativeTo(null);
         lp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.dispose();
-    }//GEN-LAST:event_jButton1MouseClicked
+    }
 
-    private void jButtonSignupMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonSignupMouseClicked
+    private void jButtonSignupMouseClicked(java.awt.event.MouseEvent evt) {
         
-    }//GEN-LAST:event_jButtonSignupMouseClicked
+    }
 
-    private void jButtonSignupActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSignupActionPerformed
+    private void jButtonSignupActionPerformed(java.awt.event.ActionEvent evt) {
         Registration rf=new Registration();
         rf.setVisible(true);
         rf.pack();
         rf.setLocationRelativeTo(null);
         rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.dispose();
-    }//GEN-LAST:event_jButtonSignupActionPerformed
+    }
 
  
     public static void main(String args[]) {
@@ -211,7 +210,6 @@ public class WelcomePage extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButtonSignup;
     private javax.swing.JLabel jLabel1;
@@ -220,5 +218,4 @@ public class WelcomePage extends javax.swing.JFrame {
     private javax.swing.JLabel jLabelMin;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    // End of variables declaration//GEN-END:variables
 }
